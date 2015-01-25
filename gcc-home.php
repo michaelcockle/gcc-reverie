a6de529ddf68b1398bc4cd3e080178e4c7514fbb
Michael Cockle <michaelcockle@Michaels-MacBook-Pro.local>
2015-01-25 15:01:31 +0000

    add slider

--- gcc-home.php
+++ gcc-home.php
@@ -8,13 +8,20 @@
 
 get_header(); ?>
 
+
 <section class="header-img">
-	<div class="header-img">
+<?php echo get_new_royalslider(1); ?>
+<!-- 	<div class="header-img">
 		<img src="http://goldsboroughcricketclub.co.uk/wp-content/themes/gcc/images/goldsborough-cricket-club-1170.jpg" alt="goldsborough cricket club">
-	</div>
+	</div> -->
 </section>
 
 <div class="row">
+
+
+
+
+
 
 	<div class="home-blog-info small-12 medium-12 large-4 columns">
 		<div class="box">
@@ -24,14 +31,27 @@ get_header(); ?>
 		</div>
 	</div>
 
+
+
+
+
 	<aside id="sidebar" class="small-12 medium-6 large-4 columns">
 		<?php dynamic_sidebar("Sidebar"); ?>
 	</aside>
+
+
+
+
 
 	<div class="small-12 medium-6 large-4 columns">
 		<a class="twitter-timeline" href="https://twitter.com/GboroCC" data-widget-id="396044343787524096">Tweets by @GboroCC</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	</div>
+
+
+
+
+
 
 
 </div>
